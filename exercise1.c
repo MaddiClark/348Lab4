0c2f8d44cc922be6f78785d2c923a2a5007e6a34
MaddiClark <cclar@ku.edu>
2026-03-01 17:26:06 -0600

    Update exercise1.c

--- exercise1.c
+++ exercise1.c
@@ -8,58 +8,52 @@ Description: Takes in two football scores and outputs all possible scoring combi
 #include <stdio.h>
 
 
-int TD2ptF(int score){
+void TD2ptF(int score){
     int output = score/8;
     printf("%d TD + 2pt, ", output);
-    return output;
 }
 
-int TD1ptF(int score){
+void TD1ptF(int score){
     int output = score/7;
     printf("%d TD + 1pt, ", output);
-    return output;
 }
 
-int TDF(int score){
+void TDF(int score){
     int output = score/6;
     printf("%d TD, ", output);
-    return output;
 }
 
-int FGF(int score){
+void FGF(int score){
     int output = score/3;
     printf("%d 3pt FG, ", output);
-    return output;
 }
 
-int SafetyF(int score){
+void SafetyF(int score){
     int output = score/2;
     printf("%d Safety", output); 
-    return output;
 }
 
 void ScoreCheck(int score){
-    int new_score = score;
     if (score > 1) {
         if (score % 8 == 0){
-           new_score = TD2ptF(score);
-           ScoreCheck(new_score-8); 
+           TD2ptF(score);
+           ScoreCheck(score-8); 
         }
         if (score % 7 == 0){
-            new_score = TD1ptF(score);
-            ScoreCheck(new_score - 7);
+            TD1ptF(score);
+            ScoreCheck(score - 7);
         }
         if (score % 6 == 0 ){
-            new_score = TDF(score);
-            ScoreCheck(new_score - 6);
+            TDF(score);
+            ScoreCheck(score - 6);
         }
         if (score % 3 == 0){
-            new_score = FGF(score);
-            ScoreCheck(new_score - 3);
+            FGF(score);
+            ScoreCheck(score - 3);
         }
         if (score % 2 == 0){
-            new_score = SafetyF(score);
-            ScoreCheck(new_score - 2);
+            SafetyF(score);
+            ScoreCheck(score - 2);
         }
     }
     printf("\n");
