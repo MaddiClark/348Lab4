d746a47c0b49727b987632ce7c3b2128e8471303
MaddiClark <cclar@ku.edu>
2026-03-01 23:35:46 -0600

    Update exercise1.c

--- exercise1.c
+++ exercise1.c
@@ -2,52 +2,53 @@
 Name: Maddi Clark
 KUID: 3162616
 Date: 3/1/2026
-Description: Takes in two football scores and outputs all possible scoring combinations
-*/
-
+Description: Takes in a football score and outputs all possible scoring combinations*/
 #include <stdio.h>
-int play[5] = {2, 3, 6, 7, 8};
-int score;
-int TD2 = 0;
-int TD1 = 0;
-int TD = 0;
-int FG = 0;
-int S = 0;
 
-void ScoreCheck(int score) {
-    if (score == 0){
-        printf("%d TD + 2pts, %d TD + FG, %d TD, %d FG, %d Safetys \n", TD2, TD1, TD, FG, S);
+int play[5] = {2, 3, 6, 7, 8};
+
+void ScoreCheck(int score, int S, int FG, int TD, int TD1, int TD2) {
+    
+    if (score < 0) {
         return;
     }
-    else
-    {
-        for (int i=0; i > 5; i++){
-            if (i == 0){
-                S +=1;
-            }
-            if (i == 1){
-                FG += 1;
-            }
-            if (i == 2){
-                TD += 1;
-            }
-            if (i == 3){
-                TD1 += 1;
-            }
-            if (i == 4){
-                TD2 += 1;
-            }
-            ScoreCheck(score - play[i]);
-        }
+
+    if (score == 0) {
+        printf("%d TD + 2pt, %d TD + FG, %d TD, %d FG, %d Safeties\n",
+               TD2, TD1, TD, FG, S);
+        return;
     }
+
+    // Safety (2)
+    ScoreCheck(score - 2, S + 1, FG, TD, TD1, TD2);
+
+    // Field Goal (3)
+    ScoreCheck(score - 3, S, FG + 1, TD, TD1, TD2);
+
+    // Touchdown (6)
+    ScoreCheck(score - 6, S, FG, TD + 1, TD1, TD2);
+
+    // TD + FG (7)
+    ScoreCheck(score - 7, S, FG, TD, TD1 + 1, TD2);
+
+    // TD + 2pt (8)
+    ScoreCheck(score - 8, S, FG, TD, TD1, TD2 + 1);
 }
 
-void main(){
-    score = 2;
-    while (score >= 2){
+int main() {
+    int score;
+
+    while (1) {
         printf("Enter 0 or 1 to STOP\n");
         printf("Enter the NFL score: ");
         scanf("%d", &score);
-        if (score > 1){ScoreCheck(score);}
+
+        if (score <= 1) {
+            break;
+        }
+
+        ScoreCheck(score, 0, 0, 0, 0, 0);
     }
+
+    return 0;
 }
